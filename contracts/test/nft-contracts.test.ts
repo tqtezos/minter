0691d0a8967fd5dbe0d1e64d69ada318bebe079e
Eugene Mishura <eugene@tqgroup.io>
2020-07-27 16:00:07 -0400

    implemented transfer test

--- contracts/test/nft-contracts.test.ts
+++ contracts/test/nft-contracts.test.ts
@@ -8,7 +8,7 @@ import {
 import { TezosToolkit, MichelsonMap } from '@taquito/taquito';
 
 
-jest.setTimeout(60000);
+jest.setTimeout(180000); // 3 minutes per test
 
 describe('initialize', () => {
   let tezos: TestTz;
@@ -71,6 +71,41 @@ describe('initialize', () => {
 
     const bobHasToken = await hasToken(bobAddress, tokenId);
     expect(bobHasToken).toBe(true);
+  })
+
+  async function transferNft(operator: TezosToolkit, from_: string, to_: string,
+    tokenId: number): Promise<void> {
+    $log.info('transferring');
+    const nftWithOperator = await operator.contract.at(nft.address);
+
+    const op = await nftWithOperator.methods.transfer([{
+      from_,
+      txs: [{ to_, token_id: tokenId, amount: 1 }]
+    }]).send();
+
+    const hash = await op.confirmation(3);
+    $log.info(`consumed gas: ${op.consumedGas}`);
+    return Promise.resolve();
+  }
+
+  test('transfer token', async () => {
+    const aliceAddress = await tezos.alice.signer.publicKeyHash();
+    const bobAddress = await tezos.bob.signer.publicKeyHash();
+    $log.info('minting')
+    const tokenId = await mintToken(tezos.bob, 'TK1', 'A token', bobAddress);
+    $log.info(`minted token ${tokenId}`);
+
+    const aliceHasATokenBefore = await hasToken(aliceAddress, tokenId);
+    const bobHasATokenBefore = await hasToken(bobAddress, tokenId);
+    expect(aliceHasATokenBefore).toBe(false);
+    expect(bobHasATokenBefore).toBe(true);
+
+    await transferNft(tezos.bob, bobAddress, aliceAddress, tokenId);
+
+    const aliceHasATokenAfter = await hasToken(aliceAddress, tokenId);
+    const bobHasATokenAfter = await hasToken(bobAddress, tokenId);
+    expect(aliceHasATokenAfter).toBe(true);
+    expect(bobHasATokenAfter).toBe(false);
   })
 
 })
