4fc4c7783b8a951be763a47317c74e9bbf3cd621
Eugene Mishura <eugene@tqgroup.io>
2020-07-27 17:34:42 -0400

    test transfer not by owner

--- contracts/test/nft-contracts.test.ts
+++ contracts/test/nft-contracts.test.ts
@@ -11,7 +11,7 @@ import { TezosToolkit, MichelsonMap } from '@taquito/taquito';
 import { LigoEnv } from './ligo';
 
 
-jest.setTimeout(180000); // 3 minutes per test
+jest.setTimeout(180000); // 3 minutes for all test
 
 describe('initialize', () => {
   let tezos: TestTz;
@@ -55,11 +55,6 @@ describe('initialize', () => {
       return Promise.reject('Invalid inspector storage state Empty.');
   }
 
-  // test('check origination', () => {
-  //   $log.debug(`minter ${minter.address}`);
-  //   $log.debug(`nft ${nft.address}`);
-  // })
-
   async function mintTokens(tz: TezosToolkit, tokens: MinterTokenMetadata[]): Promise<number[]> {
     const op = await minter.methods.mint(nft.address, tokens).send();
     const hash = await op.confirmation(3);
@@ -67,6 +62,22 @@ describe('initialize', () => {
     const storage = await minter.storage<MinterStorage>();
     return Promise.resolve(storage.last_created_token_ids);
   }
+
+  async function transferNfts(operator: TezosToolkit, txs: Fa2Transfer[]): Promise<void> {
+    $log.info('transferring');
+    const nftWithOperator = await operator.contract.at(nft.address);
+
+    const op = await nftWithOperator.methods.transfer(txs).send();
+
+    const hash = await op.confirmation(3);
+    $log.info(`consumed gas: ${op.consumedGas}`);
+    return Promise.resolve();
+  }
+
+  // test('check origination', () => {
+  //   $log.debug(`minter ${minter.address}`);
+  //   $log.debug(`nft ${nft.address}`);
+  // })
 
   test('mint token', async () => {
     const bobAddress = await tezos.bob.signer.publicKeyHash();
@@ -83,17 +94,6 @@ describe('initialize', () => {
     const [bobHasToken] = await hasTokens([{ owner: bobAddress, token_id: tokenId }]);
     expect(bobHasToken).toBe(true);
   })
-
-  async function transferNfts(operator: TezosToolkit, txs: Fa2Transfer[]): Promise<void> {
-    $log.info('transferring');
-    const nftWithOperator = await operator.contract.at(nft.address);
-
-    const op = await nftWithOperator.methods.transfer(txs).send();
-
-    const hash = await op.confirmation(3);
-    $log.info(`consumed gas: ${op.consumedGas}`);
-    return Promise.resolve();
-  }
 
   test('transfer token', async () => {
     const aliceAddress = await tezos.alice.signer.publicKeyHash();
@@ -130,5 +130,30 @@ describe('initialize', () => {
     expect(aliceHasATokenAfter).toBe(true);
     expect(bobHasATokenAfter).toBe(false);
   })
+
+  test('transfer not by owner must fail', async () => {
+    const aliceAddress = await tezos.alice.signer.publicKeyHash();
+    const bobAddress = await tezos.bob.signer.publicKeyHash();
+    $log.info('minting')
+    const [tokenId] = await mintTokens(tezos.bob,
+      [{
+        symbol: 'TK1',
+        name: 'A token',
+        owner: bobAddress,
+        extras: new MichelsonMap<string, string>()
+      }]);
+    $log.info(`minted token ${tokenId}`);
+
+    // alice is trying to transfer tokens on behalf of bob
+    const p = transferNfts(tezos.alice,
+      [
+        {
+          from_: bobAddress,
+          txs: [{ to_: aliceAddress, token_id: tokenId, amount: 1 }]
+        }
+      ]);
+
+    await expect(p).rejects.toHaveProperty('message', 'FA2_NOT_OPERATOR');
+  });
 
 })
