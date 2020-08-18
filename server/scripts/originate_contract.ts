70d45c6b4f4d037118c324b4883b76678565b059
Philip Diaz <philip@tqgroup.io>
2020-08-18 15:38:48 -0400

    feat: Complete contract origination, export to config

--- server/scripts/originate_contract.ts
+++ server/scripts/originate_contract.ts
@@ -1,18 +1,23 @@
 import { importKey } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
 import { readFileSync } from 'fs';
+import Configstore from 'configstore';
+import path from 'path';
 
-const TEZOS_RPC_URL = process.env.TEZOS_RPC_URL || 'http://localhost:20000';
+const TEZOS_RPC_URL = 'http://sandbox:20000';
+const CONFIG_NAME = 'contract-config.json';
+const CONFIG_PATH = path.join(__dirname, '../config/', CONFIG_NAME);
 
 async function main() {
-  const [, , originatorPrivateKey, storage] = process.argv;
-  const code: string = readFileSync(0).toString();
+  const [, , originatorPrivateKey, storage, configKey] = process.argv;
+  const code: string = readFileSync(0, 'utf-8').toString();
 
   if (!originatorPrivateKey || !storage) {
     console.error('Usage:');
     console.error(
       '  ts-node originate_contract.ts <originatorPrivateKey> <storage>'
     );
+    process.exit(1);
   }
 
   const Tezos = new TezosToolkit();
@@ -22,9 +27,17 @@ async function main() {
   try {
     const op = await Tezos.contract.originate({ code, init: storage });
     const contract = await op.contract(undefined, 1);
-    console.log(contract.address);
+    const config = new Configstore(
+      CONFIG_NAME,
+      {},
+      { configPath: CONFIG_PATH }
+    );
+    console.log(`Originated ${configKey} at ${contract.address}`);
+    config.set(configKey, contract.address);
+    console.log(`Updated config at server/config/${CONFIG_NAME}`);
   } catch (e) {
     console.log(e);
+    process.exit(1);
   }
 }
 
