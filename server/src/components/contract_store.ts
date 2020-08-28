14877b54894d7a686025f1560f383c3f80df498b
Philip Diaz <philip@tqgroup.io>
2020-08-28 12:18:15 -0400

    feat: Update server with new NFT contract and config

--- server/src/components/contract_store.ts
+++ server/src/components/contract_store.ts
@@ -2,13 +2,13 @@ import Configstore from 'configstore';
 import path from 'path';
 import { Contract, TezosToolkit } from '@taquito/taquito';
 
-const CONFIG_NAME = 'contract-config.json';
+const TZ_ENV = process.env.TZ_NETWORK || 'sandbox';
+const CONFIG_NAME = `minter.${TZ_ENV}.json`;
 const CONFIG_PATH = path.join(__dirname, '../../config/', CONFIG_NAME);
 
 const configStore = new Configstore('', {}, { configPath: CONFIG_PATH });
 
 export interface ContractStore {
-  minterContract(): Promise<Contract>;
   nftContract(): Promise<Contract>;
 }
 
@@ -16,12 +16,8 @@ export default async function buildContractStore(
   tzClient: TezosToolkit
 ): Promise<ContractStore> {
   return {
-    minterContract() {
-      const minterContractAddress = configStore.get('minter_contract');
-      return tzClient.contract.at(minterContractAddress);
-    },
     nftContract() {
-      const nftContractAddress = configStore.get('nft_contract');
+      const nftContractAddress = configStore.get('contracts.nft');
       return tzClient.contract.at(nftContractAddress);
     }
   };
