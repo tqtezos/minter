efcf0b550da476fe8110aca47cd0a86a6c60a033
Eugene Mishura <eugene@tqgroup.io>
2020-09-08 18:29:52 -0400

    server reads secret key form config for dev and falls back on docker storage for prod

--- server/src/components/contract_store.ts
+++ server/src/components/contract_store.ts
@@ -1,12 +1,5 @@
-import Configstore from 'configstore';
-import path from 'path';
+import { configStore } from './context';
 import { Contract, TezosToolkit } from '@taquito/taquito';
-
-const TZ_NETWORK = process.env.TZ_NETWORK;
-const CONFIG_NAME = `minter.${TZ_NETWORK}.json`;
-const CONFIG_PATH = path.join(__dirname, '../../config/', CONFIG_NAME);
-
-const configStore = new Configstore('', {}, { configPath: CONFIG_PATH });
 
 export interface ContractStore {
   nftContract(): Promise<Contract>;
