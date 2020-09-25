ac2bd3f907e6582e111055a56e4b0bc2284b24e1
Eugene Mishura <eugene@tqgroup.io>
2020-09-25 14:15:22 -0400

    refactored polling interval

--- client/src/api/contracts.ts
+++ client/src/api/contracts.ts
@@ -1,4 +1,4 @@
-import { TezosToolkit } from '@taquito/taquito';
+import { TezosToolkit, Signer } from '@taquito/taquito';
 import { InMemorySigner } from '@taquito/signer';
 
 import mkNftContract from './nftContract';
@@ -8,14 +8,23 @@ const mkContracts = async (settings: Settings) => {
   const rpc = settings.rpc;
   const nftAddress = settings.contracts.nftFaucet;
   const signer = await InMemorySigner.fromSecretKey(settings.admin.secret);
-
-  const tzClient = new TezosToolkit();
-  tzClient.setProvider({ rpc, signer });
+  const tzClient = await createToolkit(rpc, signer);
 
   return {
     nft: () => mkNftContract(tzClient, nftAddress)
-  }
+  };
+};
+
+const createToolkit = async (rpc: string, signer: Signer) => {
+  const tzClient = new TezosToolkit();
+  tzClient.setProvider({ rpc, signer });
+
+  const constants = await tzClient.rpc.getConstants();
+  const confirmationPollingIntervalSecond: number =
+    Number(constants.time_between_blocks[0]) / 5;
+  tzClient.setProvider({ config: { confirmationPollingIntervalSecond } });
+
+  return tzClient;
 };
 
 export default mkContracts;
-
--- client/src/api/nftContract.ts
+++ client/src/api/nftContract.ts
@@ -10,61 +10,59 @@ interface CreateTokenArgs {
 }
 
 export interface NftContract {
-  createToken(args: CreateTokenArgs): Promise<number>
+  createToken(args: CreateTokenArgs): Promise<number>;
 }
 
 interface NftContractStorage {
   next_token_id: string;
 }
 
-const mkNftContract =  async (
-  tzClient: TezosToolkit, 
-  address: string, 
+const mkNftContract = async (
+  tzClient: TezosToolkit,
+  address: string
 ): Promise<NftContract> => {
   const contract = await tzClient.contract.at(address);
-  
-  const constants = await tzClient.rpc.getConstants();
-  const pollingInterval: number = Number(constants.time_between_blocks[0]) / 5;
 
-  return {    
-    async createToken({ 
-        ownerAddress,
-        symbol,
-        name,
-        description,
-        ipfsCid
-      }: CreateTokenArgs
-    ): Promise<number> {
-      const storage = await contract.storage<NftContractStorage>()
-      const tokenId = storage.next_token_id
-      
-      const params = [{
-        metadata: {
-          token_id: tokenId,
-          symbol,
-          name,
-          decimals: new BigNumber(0),
-          extras: createExtras(description, ipfsCid)
-        },
-        owner: ownerAddress
-      }];
+  return {
+    async createToken({
+      ownerAddress,
+      symbol,
+      name,
+      description,
+      ipfsCid
+    }: CreateTokenArgs): Promise<number> {
+      const storage = await contract.storage<NftContractStorage>();
+      const tokenId = storage.next_token_id;
+
+      const params = [
+        {
+          metadata: {
+            token_id: tokenId,
+            symbol,
+            name,
+            decimals: new BigNumber(0),
+            extras: createExtras(description, ipfsCid)
+          },
+          owner: ownerAddress
+        }
+      ];
 
       const operation = await contract.methods.mint(params).send();
-      return operation.confirmation(1, pollingInterval);
+      return operation.confirmation(1);
     }
-  }
+  };
 };
 
-const createExtras = (description: string, ipfsCid: string) => {   
-    const extras = new MichelsonMap<string, string>({
-      prim: 'map',
-      args: [{ prim: 'string' }, { prim: 'string' }]
-    });
-      
-    extras.set('description', description);
-    extras.set('ipfs_cid', ipfsCid);
+const createExtras = (description: string, ipfsCid: string) => {
+  const extras = new MichelsonMap<string, string>({
+    prim: 'map',
+    args: [{ prim: 'string' }, { prim: 'string' }]
+  });
 
-    return extras;
+  extras.set('description', description);
+  extras.set('ipfs_cid', ipfsCid);
+
+  return extras;
 };
 
 export default mkNftContract;
