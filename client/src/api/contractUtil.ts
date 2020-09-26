143440c664ac615b19608150ca5d49dad77cd7d0
Eugene Mishura <eugene@tqgroup.io>
2020-09-26 10:16:27 -0400

    code cleanup

--- client/src/api/contractUtil.ts
+++ client/src/api/contractUtil.ts
@@ -20,18 +20,12 @@ export async function retrieveStorageField<TValue>(
 }
 
 function deepFind<TValue>(o: any, propertyName: string): TValue | undefined {
-  const deepFindImpl = (o: any) => {
-    if (o.hasOwnProperty(propertyName)) return o[propertyName];
+  if (o.hasOwnProperty(propertyName)) return o[propertyName] as TValue;
 
-    const nested = Object.values(o).filter(v => typeof v === 'object');
-    for (var n in nested) {
-      const v: any = deepFindImpl(n);
-      if (v !== undefined) return v;
-    }
-    return undefined;
-  };
-
-  const val = deepFindImpl(o);
-  if (val === undefined) return undefined;
-  return val as TValue;
+  const nested = Object.values(o).filter(v => typeof v === 'object');
+  for (var n in nested) {
+    const v: TValue | undefined = deepFind(n, propertyName);
+    if (v !== undefined) return v;
+  }
+  return undefined;
 }
--- client/src/api/nftFactoryContract.ts
+++ client/src/api/nftFactoryContract.ts
@@ -27,8 +27,7 @@ const mkNftFactoryContract = async (
 };
 
 function extractOriginatedContractAddress(op: TransactionOperation): address {
-  const result = op.results[0];
-  const txResult = result as OperationContentsAndResultTransaction;
+  const txResult = op.results[0] as OperationContentsAndResultTransaction;
   if (!txResult.metadata.internal_operation_results)
     throw new Error('Unavailable internal origination operation');
   const internalResult = txResult.metadata.internal_operation_results[0]
