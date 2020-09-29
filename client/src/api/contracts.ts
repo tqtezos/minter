dfbfee85d30256240703c9adea362623bdf2e24b
Eugene Mishura <eugene@tqgroup.io>
2020-09-29 16:46:06 -0400

    re-added nftFactory to contracts

--- client/src/api/contracts.ts
+++ client/src/api/contracts.ts
@@ -1,10 +1,12 @@
 import { TezosToolkit } from '@taquito/taquito';
 
 import mkNftContract from './nftContract';
+import mkNftFactoryContract from './nftFactoryContract';
 import { SettingsContracts } from '../generated/graphql_schema';
 
 const mkContracts = (tzToolkit: TezosToolkit, settings: SettingsContracts) => ({
-  nft: () => mkNftContract(tzToolkit, settings.nftFaucet)
+  nft: () => mkNftContract(tzToolkit, settings.nftFaucet),
+  nftFactory: () => mkNftFactoryContract(tzToolkit, settings.nftFactory)
 });
 
 export type Contracts = ReturnType<typeof mkContracts>;
