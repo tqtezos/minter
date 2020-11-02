1cd77474bcd291f773e13769d089838883241348
Alex Kuprienko <alex_kooper@Alexs-MBP.fios-router.home>
2020-11-01 19:58:21 -0500

    Feat: Validate sending token to yourself

--- client/src/components/AssetsPage/AssetTransfer.tsx
+++ client/src/components/AssetsPage/AssetTransfer.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import { jsx } from '@emotion/core';
 import { Modal, Typography, Form, Input, Button } from 'antd';
 import { keyHashValidator } from '../common/validators';
+import { useForm } from 'antd/lib/form/Form';
+import { useWalletAddress } from '../App/globalContext';
 
 const { Paragraph } = Typography;
 
@@ -12,38 +14,57 @@ interface Props {
   onOk: (values: any) => void;
 }
 
-const AssetTransfer: FC<Props> = ({ visible, onCancel, onOk }) => (
-  <Modal visible={visible} footer={null} onCancel={onCancel} width={400}>
-    <Paragraph strong css={{ textAlign: 'center' }}>
-      Where would you like to transfer this token?
-    </Paragraph>
-    <Paragraph type="secondary" css={{ textAlign: 'center' }}>
-      NOTE: All transfers are permanent
-    </Paragraph>
-    <Form layout="vertical" onFinish={onOk}>
-      <Form.Item
-        name="address"
-        label="Account"
-        css={{ marginTop: '3em' }}
-        rules={[keyHashValidator]}
-      >
-        <Input placeholder="Enter Tezos Account Here" />
-      </Form.Item>
-      <Form.Item
-        wrapperCol={{ offset: 2, span: 20 }}
-        css={{ marginTop: '5em' }}
-      >
-        <Button
-          htmlType="submit"
-          type="primary"
-          size="middle"
-          css={{ width: '100%' }}
+const AssetTransfer: FC<Props> = ({ visible, onCancel, onOk }) => {
+  const walletAddress = useWalletAddress();
+  const [form] = useForm();
+
+  const sameAddressValidator = () => ({
+    validator: (_: any, address: string) =>
+      address !== walletAddress
+        ? Promise.resolve()
+        : Promise.reject(
+            'You cannot send token to yourself! Please enter a different address'
+          )
+  });
+
+  const handleFinish = (values: any) => {
+    form.resetFields();
+    onOk(values);
+  };
+
+  return (
+    <Modal visible={visible} footer={null} onCancel={onCancel} width={400}>
+      <Paragraph strong css={{ textAlign: 'center' }}>
+        Where would you like to transfer this token?
+      </Paragraph>
+      <Paragraph type="secondary" css={{ textAlign: 'center' }}>
+        NOTE: All transfers are permanent
+      </Paragraph>
+      <Form layout="vertical" form={form} onFinish={handleFinish}>
+        <Form.Item
+          name="address"
+          label="Account"
+          css={{ marginTop: '3em' }}
+          rules={[keyHashValidator, sameAddressValidator]}
         >
-          Transfer
-        </Button>
-      </Form.Item>
-    </Form>
-  </Modal>
-);
+          <Input placeholder="Enter Tezos Account Here" />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ offset: 2, span: 20 }}
+          css={{ marginTop: '5em' }}
+        >
+          <Button
+            htmlType="submit"
+            type="primary"
+            size="middle"
+            css={{ width: '100%' }}
+          >
+            Transfer
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
 
 export default AssetTransfer;
