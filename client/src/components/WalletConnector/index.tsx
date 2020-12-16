b1cb84e90079731f7919b7ca197e23f3210c3894
Philip Diaz <philip@tqgroup.io>
2020-12-16 16:37:05 -0500

    chore: Remove specific Thanos wallet client implementation

--- client/src/components/WalletConnector/index.tsx
+++ client/src/components/WalletConnector/index.tsx
@@ -6,7 +6,6 @@ import { message } from 'antd';
 import HeaderButton from '../common/HeaderButton';
 import { useTzToolkit, useTzToolkitSetter } from '../App/globalContext';
 import useSettings from '../common/useSettings';
-import * as thanosWallet from './thanosWallet';
 import * as beaconWallet from './beaconWallet';
 
 const WalletConnector: FC = () => {
@@ -25,9 +24,7 @@ const WalletConnector: FC = () => {
       const tzToolkit = await beaconWallet.connect(rpc, bcdNetwork);
       setTzToolkit(tzToolkit);
     } catch (err) {
-      if (err.name === 'NotGrantedThanosWalletError')
-        message.error('Transaction rejected');
-      else message.error(err.message);
+      message.error(err.message);
     } finally {
       setConnecting(false);
     }
