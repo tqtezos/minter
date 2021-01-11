3b3a6c96e6501e808c80d73ae707f5bf8a1b9fc3
Philip Diaz <philip@tqgroup.io>
2021-01-11 16:38:20 -0500

    feat: Add dynamic metadata fields

--- client/src/components-v2/CreateNonFungiblePage/Form.tsx
+++ client/src/components-v2/CreateNonFungiblePage/Form.tsx
@@ -72,36 +72,66 @@ export default function Form({
       <Heading size="md" paddingBottom={6}>
         Create new values for your asset
       </Heading>
-      <Flex align="space-between">
-        <FormControl paddingBottom={6} paddingRight={2}>
-          <FormLabel fontFamily="mono">Name</FormLabel>
-          <Input placeholder="e.g. Country" />
-        </FormControl>
-        <FormControl paddingBottom={6} paddingLeft={2}>
-          <FormLabel fontFamily="mono">Value</FormLabel>
-          <Input placeholder="e.g. India" />
-        </FormControl>
-      </Flex>
-      <Flex align="space-between">
-        <FormControl paddingBottom={6} paddingRight={2}>
-          <FormLabel fontFamily="mono">Name</FormLabel>
-          <Input placeholder="e.g. Country" />
-        </FormControl>
-        <FormControl paddingBottom={6} paddingLeft={2}>
-          <FormLabel fontFamily="mono">Value</FormLabel>
-          <Input placeholder="e.g. India" />
-        </FormControl>
-      </Flex>
-      <Flex align="space-between">
-        <FormControl paddingBottom={6} paddingRight={2}>
-          <FormLabel fontFamily="mono">Name</FormLabel>
-          <Input placeholder="e.g. Country" />
-        </FormControl>
-        <FormControl paddingBottom={6} paddingLeft={2}>
-          <FormLabel fontFamily="mono">Value</FormLabel>
-          <Input placeholder="e.g. India" />
-        </FormControl>
-      </Flex>
+      {state.metadataRows.map(({ name, value }, i) => {
+        return (
+          <Flex key={i} align="center" justify="stretch">
+            <FormControl paddingBottom={6} paddingRight={2} flex="1">
+              <FormLabel fontFamily="mono">Name</FormLabel>
+              <Input
+                placeholder="e.g. Country"
+                value={name || ''}
+                onChange={e =>
+                  dispatch({
+                    type: 'update_metadata_row_name',
+                    payload: { key: i, name: e.target.value }
+                  })
+                }
+              />
+            </FormControl>
+            <FormControl paddingBottom={6} paddingLeft={2} flex="1">
+              <FormLabel fontFamily="mono">Value</FormLabel>
+              <Input
+                placeholder="e.g. India"
+                value={value || ''}
+                onChange={e =>
+                  dispatch({
+                    type: 'update_metadata_row_value',
+                    payload: { key: i, value: e.target.value }
+                  })
+                }
+              />
+            </FormControl>
+            <Flex
+              h={6}
+              w={6}
+              color="brand.red"
+              justify="center"
+              align="center"
+              borderRadius="50%"
+              borderWidth="1px"
+              borderColor="brand.red"
+              fontSize="xs"
+              fontWeight="600"
+              ml={3}
+              mt={1}
+              cursor="pointer"
+              onClick={() =>
+                dispatch({ type: 'delete_metadata_row', payload: { key: i } })
+              }
+            >
+              X
+            </Flex>
+          </Flex>
+        );
+      })}
+      <Text
+        textDecor="underline"
+        color="brand.blue"
+        cursor="pointer"
+        onClick={() => dispatch({ type: 'add_metadata_row' })}
+      >
+        + Add new field
+      </Text>
     </>
   );
 }
--- client/src/components-v2/CreateNonFungiblePage/reducer.ts
+++ client/src/components-v2/CreateNonFungiblePage/reducer.ts
@@ -1,15 +1,5 @@
 import { Dispatch } from 'react';
-
-export type Action =
-  | { type: 'increment_step' }
-  | { type: 'decrement_step' }
-  | { type: 'update_field'; payload: { name: string; value: string } }
-  | { type: 'add_metadata_row' }
-  | {
-      type: 'update_metadata_row';
-      payload: { key: number; name: string; value: string };
-    }
-  | { type: 'select_collection'; payload: { address: string } };
+import produce from 'immer';
 
 type Step = 'file_upload' | 'asset_details' | 'collection_select';
 
@@ -19,16 +9,34 @@ export const steps: Step[] = [
   'collection_select'
 ];
 
+interface Fields {
+  name: string | null;
+  description: string | null;
+  ipfs_hash: string | null;
+}
+
 export interface State {
   step: Step;
-  fields: {
-    name: string | null;
-    description: string | null;
-    ipfs_hash: string | null;
-  };
+  fields: Fields;
   metadataRows: { name: string | null; value: string | null }[];
   collectionAddress: string | null;
 }
+
+export type Action =
+  | { type: 'increment_step' }
+  | { type: 'decrement_step' }
+  | { type: 'update_field'; payload: { name: keyof Fields; value: string } }
+  | { type: 'add_metadata_row' }
+  | {
+      type: 'update_metadata_row_name';
+      payload: { key: number; name: string };
+    }
+  | {
+      type: 'update_metadata_row_value';
+      payload: { key: number; value: string };
+    }
+  | { type: 'delete_metadata_row'; payload: { key: number } }
+  | { type: 'select_collection'; payload: { address: string } };
 
 export type DispatchFn = Dispatch<Action>;
 
@@ -48,24 +56,60 @@ export function reducer(state: State, action: Action) {
     case 'increment_step': {
       const stepIdx = steps.indexOf(state.step);
       if (stepIdx + 1 < steps.length) {
-        return { ...state, step: steps[stepIdx + 1] };
+        return produce(state, draftState => {
+          draftState.step = steps[stepIdx + 1];
+        });
       }
       return state;
     }
     case 'decrement_step': {
       const stepIdx = steps.indexOf(state.step);
       if (stepIdx > 0) {
-        return { ...state, step: steps[stepIdx - 1] };
+        return produce(state, draftState => {
+          draftState.step = steps[stepIdx - 1];
+        });
       }
       return state;
     }
     case 'update_field': {
       const { name, value } = action.payload;
-      return { ...state, fields: { ...state.fields, [name]: value } };
+      return produce(state, draftState => {
+        draftState.fields[name] = value;
+      });
+    }
+    case 'add_metadata_row': {
+      return produce(state, draftState => {
+        draftState.metadataRows.push({ name: null, value: null });
+      });
+    }
+    case 'update_metadata_row_name': {
+      const { key, name } = action.payload;
+      if (!state.metadataRows[key]) {
+        return state;
+      }
+      return produce(state, draftState => {
+        draftState.metadataRows[key].name = name;
+      });
+    }
+    case 'update_metadata_row_value': {
+      const { key, value } = action.payload;
+      if (!state.metadataRows[key]) {
+        return state;
+      }
+      return produce(state, draftState => {
+        draftState.metadataRows[key].value = value;
+      });
+    }
+    case 'delete_metadata_row': {
+      return produce(state, draftState => {
+        draftState.metadataRows.splice(action.payload.key, 1);
+      });
     }
     case 'select_collection': {
       const { address } = action.payload;
-      return { ...state, collectionAddress: address };
+      return produce(state, draftState => {
+        draftState.collectionAddress = address;
+      });
     }
     default:
       return state;
