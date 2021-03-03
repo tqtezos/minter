6a18f291216f818d05d7894e65d3ae3f13bb86bd
Philip Diaz <philip.diaz@tqtezos.com>
2021-03-03 16:40:30 -0500

    refactor: Update bootstrap-contracts-config script with clearer functions

--- scripts/bootstrap-contracts-config.ts
+++ scripts/bootstrap-contracts-config.ts
@@ -4,96 +4,40 @@ import { $log } from '@tsed/logger';
 import axios from 'axios';
 import retry from 'async-retry';
 import Configstore from 'configstore';
-import {
-  MichelsonMap,
-  TezosToolkit,
-  ContractAbstraction,
-  ContractProvider
-} from '@taquito/taquito';
+import { MichelsonMap, TezosToolkit } from '@taquito/taquito';
 import { InMemorySigner } from '@taquito/signer';
+import { OriginationOperation } from '@taquito/taquito/dist/types/operations/origination-operation';
 
-async function loadFile(fileName: string): Promise<string> {
-  return new Promise<string>((resolve, reject) =>
-    fs.readFile(fileName, (err, buff) =>
-      err ? reject(err) : resolve(buff.toString())
-    )
-  );
-}
+// Client & Server Config Generation
 
-function genClientConfig(mainConfig: Configstore) {
+function genClientConfig(rootConfig: Configstore) {
   const configPath = path.join(__dirname, `../client/src/config.json`);
   const clientConfig = new Configstore('client', {}, { configPath });
-
   const clientConfigKeys = ['rpc', 'network', 'bcd', 'ipfs', 'contracts'];
 
   for (let key of clientConfigKeys) {
-    clientConfig.set(key, mainConfig.get(key));
+    clientConfig.set(key, rootConfig.get(key));
   }
 }
 
-function genServerConfig(mainConfig: Configstore) {
+function genServerConfig(rootConfig: Configstore) {
   const configPath = path.join(__dirname, `../server/src/config.json`);
-  const clientConfig = new Configstore('server', {}, { configPath });
+  const serverConfig = new Configstore('server', {}, { configPath });
+  const serverConfigKeys = ['pinata'];
 
-  const clientConfigKeys = ['pinata'];
-
-  for (let key of clientConfigKeys) {
-    clientConfig.set(key, mainConfig.get(key));
+  for (let key of serverConfigKeys) {
+    serverConfig.set(key, rootConfig.get(key));
   }
-}
-
-function getEnv(): string {
-  const env = process.env['TZ_NETWORK'];
-  if (!env) {
-    $log.error(`TZ_NETWORK environment variable is not set`);
-    process.exit(1);
-  }
-  return env;
-}
-
-function getConfig(env: string): Configstore {
-  const configFileName = path.join(__dirname, `../config/minter.${env}.json`);
-  if (!fs.existsSync(configFileName)) {
-    $log.error(`Environment config file ${configFileName} does not exist`);
-    process.exit(1);
-  }
-  return new Configstore('minter', {}, { configPath: configFileName });
 }
 
 function toHexString(input: string) {
   return Buffer.from(input).toString('hex');
 }
 
-async function createToolkit(config: Configstore): Promise<TezosToolkit> {
-  const key = config.get('admin.secret');
-  const signer = await InMemorySigner.fromSecretKey(key);
-  const rpc = config.get('rpc');
-  if (!rpc) throw new Error('cannot read node rpc');
-
-  const toolkit = new TezosToolkit(rpc);
-  toolkit.setProvider({
-    signer,
-    rpc,
-    config: { confirmationPollingIntervalSecond: 5 }
-  });
-  return toolkit;
-}
-
-async function awaitForNetwork(tz: TezosToolkit): Promise<void> {
-  $log.info('connecting to network...');
-
-  await retry(async () => await tz.rpc.getBlockHeader({ block: '2' }), {
-    retries: 8
-  });
-
-  $log.info('connected');
-}
-
 export async function originateNftFaucet(
-  tz: TezosToolkit,
+  toolkit: TezosToolkit,
   code: string
-): Promise<ContractAbstraction<ContractProvider>> {
-  const name = 'nft_faucet_main';
+): Promise<OriginationOperation> {
   const metadata = new MichelsonMap<string, string>();
   const contents = {
     name: 'Minter',
@@ -103,88 +47,127 @@ export async function originateNftFaucet(
   };
   metadata.set('', toHexString('tezos-storage:contents'));
   metadata.set('contents', toHexString(JSON.stringify(contents)));
-  try {
-    const originationOp = await tz.contract.originate({
-      code: code,
-      storage: {
-        assets: {
-          ledger: new MichelsonMap(),
-          next_token_id: 0,
-          operators: new MichelsonMap(),
-          token_metadata: new MichelsonMap()
-        },
-        metadata: metadata
-      }
-    });
-
-    const contract = await originationOp.contract();
-    $log.info(`originated contract ${name} with address ${contract.address}`);
-    $log.info(`consumed gas: ${originationOp.consumedGas}`);
-    return Promise.resolve(contract);
-  } catch (error) {
-    const jsonError = JSON.stringify(error, null, 2);
-    $log.fatal(`${name} origination error ${jsonError}`);
-    return Promise.reject(error);
-  }
+  return await toolkit.contract.originate({
+    code: code,
+    storage: {
+      assets: {
+        ledger: new MichelsonMap(),
+        next_token_id: 0,
+        operators: new MichelsonMap(),
+        token_metadata: new MichelsonMap()
+      },
+      metadata: metadata
+    }
+  });
 }
 
-async function findConfigContract(
+async function exitOnExistingBootstrap(
   config: Configstore,
-  tz: TezosToolkit,
+  toolkit: TezosToolkit,
   configKey: string
-): Promise<string | null> {
+): Promise<void> {
   const address = config.get(configKey);
-  if (!address) return null;
+  if (!address) return;
 
   try {
-    await tz.contract.at(address);
-    return address;
-  } catch (e) {
-    return null;
-  }
-}
-
-async function bootstrapNftFaucet(
-  config: Configstore,
-  tz: TezosToolkit
-): Promise<void> {
-  const configKey = 'contracts.nftFaucet';
-  const configContract = await findConfigContract(config, tz, configKey);
-  if (configContract) {
+    await toolkit.contract.at(address);
     $log.info(
-      `Contract already exists at ${configContract}. Skipping origination...`
+      `Contract already exists at address ${address}. Skipping origination`
     );
     process.exit(0);
+  } catch (e) {
+    return;
   }
+}
 
-  $log.info('originating...');
+async function fetchFaucetContractCode() {
   const rawRepoUrl = 'https://raw.githubusercontent.com/tqtezos/minter-sdk';
   const gitHash = '8f67bb8c2abc12b8e6f8e529e1412262972deab3';
-  const resp = await axios.get(
-    `${rawRepoUrl}/${gitHash}/contracts/bin/fa2_multi_nft_faucet.tz`
-  );
-  const code = resp.data;
-  const contract = await originateNftFaucet(tz, code);
+  const contractCodeUrl = `${rawRepoUrl}/${gitHash}/contracts/bin/fa2_multi_nft_faucet.tz`;
+  const response = await axios.get(contractCodeUrl);
+  return { code: response.data, url: contractCodeUrl };
+}
+
+async function waitForNetwork(toolkit: TezosToolkit): Promise<void> {
+  await retry(async () => await toolkit.rpc.getBlockHeader({ block: '2' }), {
+    retries: 8
+  });
+}
+
+async function createToolkit(config: Configstore): Promise<TezosToolkit> {
+  const rpc = config.get('rpc');
+  const key = config.get('admin.secret');
+  const signer = await InMemorySigner.fromSecretKey(key);
+  const toolkit = new TezosToolkit(rpc);
+  const providerConfig = { confirmationPollingIntervalSecond: 5 };
+  toolkit.setProvider({ signer, rpc, config: providerConfig });
+  return toolkit;
+}
+
+function readConfig(env: string): Configstore {
+  const configFileName = path.join(__dirname, `../config/minter.${env}.json`);
+  if (!fs.existsSync(configFileName)) {
+    $log.error(`Environment config file ${configFileName} does not exist`);
+    process.exit(1);
+  }
+  return new Configstore('minter', {}, { configPath: configFileName });
+}
+
+function readEnv(): string {
+  const env = process.env['TZ_NETWORK'];
+  if (!env) {
+    $log.error(`TZ_NETWORK environment variable is not set`);
+    process.exit(1);
+  }
+  return env;
+}
+
+async function bootstrap(env: string) {
+  $log.info(`Bootstrapping ${env} environment config...`);
+  const configKey = 'contracts.nftFaucet';
+  const config = readConfig(env);
+  const toolkit = await createToolkit(config);
+
+  $log.info('Connecting to network...');
+  await waitForNetwork(toolkit);
+  $log.info('Connected');
+
+  // Exit the script if the contract address defined in the configuration
+  // already exists on chain
+  await exitOnExistingBootstrap(config, toolkit, configKey);
+
+  let contract;
+  try {
+    const { code, url: contractCodeUrl } = await fetchFaucetContractCode();
+
+    $log.info(`Originating contract from ${contractCodeUrl} ...`);
+
+    const origOp = await originateNftFaucet(toolkit, code);
+    contract = await origOp.contract();
+
+    $log.info(`Originated nftFaucet contract at address ${contract.address}`);
+    $log.info(`  Consumed gas: ${origOp.consumedGas}`);
+  } catch (error) {
+    const jsonError = JSON.stringify(error, null, 2);
+    $log.error(`nftFaucet origination error ${jsonError}`);
+    process.exit(1);
+  }
+
   config.set(configKey, contract.address);
-  $log.info('originated');
+  $log.info(`Updated configuration`);
+
+  genClientConfig(config);
+  genServerConfig(config);
+
+  process.exit(0);
 }
 
 async function main() {
-  const env = getEnv();
+  const env = readEnv();
   try {
-    $log.info(`bootstrapping ${env} environment config...`);
-
-    const config = getConfig(env);
-    const toolkit = await createToolkit(config);
-    await awaitForNetwork(toolkit);
-    await bootstrapNftFaucet(config, toolkit);
-
-    genClientConfig(config);
-    genServerConfig(config);
-
-    process.exit(0);
+    await bootstrap(env);
   } catch (err) {
-    $log.error(`error while bootstrapping environment ${env}`);
+    $log.error(`Error while bootstrapping environment ${env}`);
     $log.error(err);
     process.exit(1);
   }
