fdd3233eda7e572a47f6b5cad333778e2bfa9ec2
Philip Diaz <philip.diaz@tqtezos.com>
2021-03-03 16:42:10 -0500

    chore: Remove star imports from bootstrap-contracts-config

--- scripts/bootstrap-contracts-config.ts
+++ scripts/bootstrap-contracts-config.ts
@@ -1,5 +1,5 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import fs from 'fs';
+import path from 'path';
 import { $log } from '@tsed/logger';
 import axios from 'axios';
 import retry from 'async-retry';
