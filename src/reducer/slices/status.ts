a4089b7c74369f32b8a63e770f8074fdbda08e8d
Brandon Barber <roecrew@gmail.com>
2021-03-22 11:41:04 -0600

    QueueModal useEffect/UI updates

--- src/reducer/slices/status.ts
+++ src/reducer/slices/status.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, SerializedError } from '@reduxjs/toolkit';
+import { AsyncThunk, createSlice, PayloadAction, SerializedError } from '@reduxjs/toolkit';
 import {
   createAssetContractAction,
   mintTokenAction,
