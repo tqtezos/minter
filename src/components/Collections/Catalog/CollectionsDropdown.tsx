e4c6e07728b66fa936ac58ca665870f0d691883f
Philip Diaz <philip.diaz@tqtezos.com>
2021-03-26 15:20:12 -0400

    feat: Add collections dropdown

---
+++ src/components/Collections/Catalog/CollectionsDropdown.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuItemOption,
+  Text
+} from '@chakra-ui/react';
+import { useSelector, useDispatch } from '../../../reducer';
+import { selectCollection } from '../../../reducer/slices/collections';
+import { ChevronDown } from 'react-feather';
+
+export default function CollectionsDropdown() {
+  const state = useSelector(s => s.collections);
+  const dispatch = useDispatch();
+
+  return (
+    <Menu>
+      <MenuButton as={Button} border="1px solid" borderColor="brand.gray">
+        <Flex align="center">
+          <Box mr={3}>
+            <ChevronDown />
+          </Box>
+          Collections
+        </Flex>
+      </MenuButton>
+      <MenuList>
+        <MenuOptionGroup
+          type="radio"
+          defaultValue={state.selectedCollection || ''}
+        >
+          <Text ml={4} my={2} fontWeight="600">
+            Featured
+          </Text>
+          <MenuItemOption
+            key={state.globalCollection}
+            selected={state.globalCollection === state.selectedCollection}
+            value={state.globalCollection}
+            onSelect={() => dispatch(selectCollection(state.globalCollection))}
+          >
+            {state.collections[state.globalCollection].metadata.name}
+          </MenuItemOption>
+          <Text ml={4} my={2} fontWeight="600">
+            Your Collections
+          </Text>
+          {Object.keys(state.collections)
+            .filter(address => address !== state.globalCollection)
+            .map(address => (
+              <MenuItemOption
+                key={address}
+                value={address}
+                selected={address === state.selectedCollection}
+                onClick={() => dispatch(selectCollection(address))}
+              >
+                {state.collections[address].metadata.name}
+              </MenuItemOption>
+            ))}
+        </MenuOptionGroup>
+      </MenuList>
+    </Menu>
+  );
+}
