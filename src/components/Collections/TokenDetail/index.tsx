9423f9de6d6c4567fdb43b8bea5755241f6668f9
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-03-29 21:14:47 -0300

    Fix back navigation

--- src/components/Collections/TokenDetail/index.tsx
+++ src/components/Collections/TokenDetail/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'wouter';
 import {
   Accordion,
   AccordionButton,
@@ -156,7 +155,6 @@ interface TokenDetailProps {
 }
 
 function TokenDetail({ contractAddress, tokenId }: TokenDetailProps) {
-  const [, setLocation] = useLocation();
   const { system, collections: state } = useSelector(s => s);
   const disclosure = useDisclosure();
   const dispatch = useDispatch();
@@ -297,13 +295,12 @@ function TokenDetail({ contractAddress, tokenId }: TokenDetailProps) {
           variant="primaryActionInverted"
           onClick={e => {
             e.preventDefault();
-            setLocation('/collections', { replace: true });
+            window.history.back();
           }}
         >
           <Box color="currentcolor">
             <ChevronLeft size={16} strokeWidth="3" />
           </Box>
-          <Text ml={2}>Collections</Text>
         </MinterButton>
       </Flex>
       <Flex
