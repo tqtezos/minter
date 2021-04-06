0e115ceebfe948f3ac5d63f867edae0cea9703a6
Brandon Barber <roecrew@gmail.com>
2021-04-06 12:46:39 -0700

    *

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    webpack: (config, options) => {
+      config.module.rules.push({
+            test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+            loader: 'url-loader?limit=100000' })
+      return config;
+    },
+  }
