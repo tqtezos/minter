612a3bc06a334c52256d4515e7fb12d0ad6322e7
maelswarm <brandonjbarber@protonmail.com>
2021-04-23 07:17:00 -0700

    *

--- src/components/Marketplace/Catalog/FeaturedToken.tsx
+++ src/components/Marketplace/Catalog/FeaturedToken.tsx
@@ -11,7 +11,6 @@ import {
   Heading,
   Stack
 } from '@chakra-ui/react';
-import { MinterButton } from '../../common';
 import { TokenMedia } from '../../common/TokenMedia';
 
 interface FeaturedTokenProps extends Token {
@@ -22,16 +21,24 @@ export default function FeaturedToken(props: FeaturedTokenProps) {
   const [, setLocation] = useLocation();
   return (
     <Container maxW="100%" py={10} px={0}>
-      <Stack width="100%" direction={{ base: 'column', md: 'row' }} spacing="24px" mb={10} display="flex" flexDir="row" flexWrap="wrap" justifyContent="center" alignItems="center">
-        <Flex pr={[0, 10]} borderRight={["unset", "2px solid #666"]}>
+      <Stack width="100%" direction={{ base: 'column', md: 'row' }} spacing="24px" mb={10} display="flex" flexDir="row" flexWrap="wrap" justifyContent="center" alignItems="center"
+        onClick={e => {
+          e.preventDefault();
+          setLocation(`/collection/${props.address}/token/${props.id}`, {
+            replace: false
+          });
+        }}>
+          <Flex paddingRight={[0, 10]} borderRight={['', '2px solid #aaa']} justifyContent="flex-end">
           <TokenMedia
             src={ipfsUriToGatewayUrl(props.network, props.artifactUri)}
-            maxW="calc(650px - 2.5rem)"
+            height="auto"
+            maxW="60vw"
+            maxH="60vh"
           />
-        </Flex>
+          </Flex>
         <Box pl={[0, 10]} marginLeft="0 !important">
           <Flex flexDir="column" h="100%" w="100%">
-            <Heading size="md" mt={4} fontSize="2.5rem">
+            <Heading size="lg" mt={4} fontSize="2.5rem">
               {props.title}
             </Heading>
             <Heading size="sm" my={4} color="brand.darkGray">
@@ -50,20 +57,6 @@ export default function FeaturedToken(props: FeaturedTokenProps) {
                 {props.sale?.price} ꜩ
               </Text>
             </Text>
-            <MinterButton
-              size="md"
-              variant="primaryAction"
-              w="150px"
-              my={4}
-              onClick={e => {
-                e.preventDefault();
-                setLocation(`/collection/${props.address}/token/${props.id}`, {
-                  replace: false
-                });
-              }}
-            >
-              <Text>View</Text>
-            </MinterButton>
           </Flex>
         </Box>
       </Stack>
--- src/components/common/TokenMedia.tsx
+++ src/components/common/TokenMedia.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { FiHelpCircle } from 'react-icons/fi';
 
 function MediaNotFound() {
@@ -18,9 +18,10 @@ function MediaNotFound() {
   );
 }
 
-export function TokenMedia(props: { src: string; maxW?: string }) {
+export function TokenMedia(props: { src: string, maxW?: string, maxH?: string, onLoad?: Function, height?: string }) {
   const [errored, setErrored] = useState(false);
   const [obj, setObj] = useState<{ url: string; type: string } | null>(null);
+
   useEffect(() => {
     (async () => {
       let blob;
@@ -44,14 +45,12 @@ export function TokenMedia(props: { src: string; maxW?: string }) {
 
   if (/^image\/.*/.test(obj.type)) {
     return (
-      <Image
-        src={props.src}
-        objectFit="scale-down"
-        height="100%"
-        flex="1"
-        maxWidth={props.maxW}
-        style={{ objectFit: 'scale-down' }}
+      <img
+        src={obj.url}
+        style={{ objectFit: "scale-down", maxWidth: props.maxW ?? '100%', height: props.height ?? "100%", maxHeight: props.maxH ?? '100%'}}
         onError={() => setErrored(true)}
+        onLoad={() => { if (props.onLoad && obj?.url) { props.onLoad(obj.url, obj.type) }; }}
+        alt=""
       />
     );
   }
@@ -63,8 +62,8 @@ export function TokenMedia(props: { src: string; maxW?: string }) {
         onClick={e => e.preventDefault()}
         onMouseEnter={e => e.currentTarget.play()}
         onMouseLeave={e => e.currentTarget.pause()}
-        height="100%"
-        style={{ maxWidth: props.maxW }}
+        style={{ objectFit: "cover", width: '100%', maxWidth: props.maxW ?? '100%', height: props.height ?? "100%", maxHeight: props.maxH ?? '100%' }}
+        onLoadedMetadata={() => { if (props.onLoad && obj?.url) { props.onLoad(obj.url, obj.type) } }}
       >
         <source src={obj.url} type={obj.type} />
       </video>
