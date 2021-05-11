757e89fa7cd78d08eb0b27c19f71d05b08a72178
maelswarm <roecrew@gmail.com>
2021-05-11 13:00:04 -0400

    router clean-up

--- src/components/CreateNonFungiblePage/index.tsx
+++ src/components/CreateNonFungiblePage/index.tsx
@@ -119,7 +119,7 @@ export default function CreateNonFungiblePage() {
               variant="cancelAction"
               onClick={() => {
                 dispatch(clearForm());
-                setLocation('/collections');
+                setLocation('/creator');
               }}
               display="flex"
               alignItems="center"
@@ -176,7 +176,7 @@ export default function CreateNonFungiblePage() {
               method="mintToken"
               dispatchThunk={() => dispatch(mintTokenAction())}
               onComplete={() => dispatch(clearForm())}
-              afterClose={() => setLocation('/collections')}
+              afterClose={() => setLocation('/creator')}
               dispatchOnOpen={true}
               pendingAsyncMessage={
                 <>
